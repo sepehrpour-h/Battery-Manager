03dec3241e535dad3cacf7054b25c70ab09bbed4
sepehrpour-h <hamidreza.sepehrpour@gmail.com>
2023-08-16 13:51:31 +0330

    session 65

--- app/src/main/java/ir/mytehran/batterymanager/activity/MainActivity.kt
+++ app/src/main/java/ir/mytehran/batterymanager/activity/MainActivity.kt
@@ -1,14 +1,17 @@
 package ir.mytehran.batterymanager.activity
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
+import ir.mytehran.batterymanager.R
 import ir.mytehran.batterymanager.utils.BatteryUsage
 import ir.mytehran.batterymanager.databinding.ActivityMainBinding
 import ir.mytehran.batterymanager.model.BatteryModel
@@ -23,11 +26,11 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.imgMenu.setOnClickListener{
+        binding.imgMenu.setOnClickListener {
             binding.drawer.openDrawer(Gravity.RIGHT)
         }
 
-        binding.incDrawer.txtAppUsage.setOnClickListener{
+        binding.incDrawer.txtAppUsage.setOnClickListener {
             startActivity(Intent(this@MainActivity, UsageBatteryActivity::class.java))
             binding.drawer.closeDrawer(Gravity.RIGHT)
         }
@@ -36,6 +39,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private var batteryInfoReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        @SuppressLint("SetTextI18n")
         override fun onReceive(context: Context, intent: Intent) {
             var batteryLevel = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
 
@@ -55,6 +59,45 @@ class MainActivity : AppCompatActivity() {
             binding.circularProgressBar.progressMax = 100f
             binding.circularProgressBar.setProgressWithAnimation(batteryLevel.toFloat())
             binding.txtCharge.text = batteryLevel.toString() + "%"
+
+            val health = intent.getIntExtra(BatteryManager.EXTRA_HEALTH, 0)
+            when (health) {
+                BatteryManager.BATTERY_HEALTH_DEAD -> {
+                    binding.txtHealth.text =    "your battery is fully dead, please change your battery"
+                    binding.txtHealth.setTextColor(Color.parseColor("#000000"))
+                    binding.imgHealth.setImageResource(R.drawable.health_dead)
+                }
+
+                BatteryManager.BATTERY_HEALTH_GOOD -> {
+                    binding.txtHealth.text =    "your battery is good, please take care of that"
+                    binding.txtHealth.setTextColor(Color.GREEN)
+                    binding.imgHealth.setImageResource(R.drawable.health_good)
+                }
+
+                BatteryManager.BATTERY_HEALTH_COLD -> {
+                    binding.txtHealth.text =    "your battery is cold, it's ok"
+                    binding.txtHealth.setTextColor(Color.BLUE)
+                    binding.imgHealth.setImageResource(R.drawable.health_cold)
+                }
+
+                BatteryManager.BATTERY_HEALTH_OVERHEAT -> {
+                    binding.txtHealth.text =    "your battery is overheat, please don't work with your phone"
+                    binding.txtHealth.setTextColor(Color.RED)
+                    binding.imgHealth.setImageResource(R.drawable.health_overheat)
+                }
+
+                BatteryManager.BATTERY_HEALTH_OVER_VOLTAGE -> {
+                    binding.txtHealth.text =    "your battery is over voltage, please don't work with your phone"
+                    binding.txtHealth.setTextColor(Color.YELLOW)
+                    binding.imgHealth.setImageResource(R.drawable.health_volt)
+                }
+
+                else -> {
+                    binding.txtHealth.text =    "your battery is fully dead, please change your battery"
+                    binding.txtHealth.setTextColor(Color.parseColor("#000000"))
+                    binding.imgHealth.setImageResource(R.drawable.health_dead)
+                }
+            }
         }
     }
 }
